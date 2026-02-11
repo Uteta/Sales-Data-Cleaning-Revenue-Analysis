4642dad5bf31f28b52650232dc50ded23f1ab80a
Uteta <150948303+Uteta@users.noreply.github.com>
2026-02-11 14:31:37 -0500

    Update data_cleaning.py
    Updated code with clean variable names and comments

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,24 +1,53 @@
+"""
+Sales Data Cleaning & Revenue Analysis
+Author: Uwase Laetitia Uteta
+Description: This script load raw sales data,cleans it,and creates a revenue column for analysis with basic insights.
+"""
 import pandas as pd
 
-# Load data set
+# ------------------------------
+# 1. Load data set
+# ------------------------------
 df = pd.read_csv("sales_data.csv")
 
 # Display first 5 rows
+print("First 5 rows of raw data:")
 print(df.head())
 
-# check missing values
-print(df.isnull().sum())
+# -------------------------------
+# 2. Handle  missing values
+# -------------------------------
+# Fill missing Quantity or Price with 0
+df['Quantity'] = df['Quantity'].fillna(0)
+df['Price'] = df['Price'].fillna(0)
 
-#remove duplicates
+# Drop rows with missing Order_Date
+df = df.dropna(subset=['Order_Date'])
+
+# -------------------------------
+# 3. Standardize column names
+# -------------------------------
+df.columns = df.columns.str.strip().str.replace(' ', '_').str.lower()
+
+# -------------------------------
+# 4. Remove duplicate records
+# -------------------------------
 df = df.drop_duplicates()
 
-#convert date column
-df['Order_Date'] = pd.to_datetime(df['Order_Date'])
+# -------------------------------
+# 5. Convert date column
+# -------------------------------
+df['order_date'] = pd.to_datetime(df['order_date'])
 
-#Create revenue column
-df['Revenue'] = df['Quantity'] * df['Price']
+# -------------------------------
+# 6. Create revenue column
+# -------------------------------
+df['revenue'] = df['quantity'] * df['price']
 
-#save cleaned file
+# -------------------------------
+# 7. Save cleaned dataset
+# -------------------------------
 df.to_csv("cleaned_sales_data.csv", index=False)
+print("Cleanded data saved to 'cleaned_sales_data.csv'.")
 
-print("Data cleaning complete.")
+
